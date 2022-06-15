b7c4d0d5989b80f0df02f9ef5f3470cb6e2a28ae
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-06-15 03:08:05 +0100

    updated settings.py

--- bosun-django/Bosun/Bosun/settings.py
+++ bosun-django/Bosun/Bosun/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nz1%coffdmyu(&xq(n7j(v^9v3!(y_2g8^09s_k$0dm@vmajj!'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

0ea4088dd7b4bcc696bee64a58c2bdea64a700fc
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-07-14 23:24:27 +0100

    updated main site and app urls.py files

--- django-crud/I4G0073943XH/settings.py
+++ django-crud/I4G0073943XH/settings.py
@@ -17,6 +17,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'I4G0073943XH.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

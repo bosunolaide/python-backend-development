f6056254dc9a1d9accd22aa8623a79531f288bd0
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-07-20 01:44:00 +0100

    updated models.py

--- django-querysets-and-managers/links/models.py
+++ django-querysets-and-managers/links/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
 from django.utils import timezone
+
+from links.managers import ActiveLinkManager
 # Create your models here.
 
 class Link(models.Model):
@@ -12,6 +14,8 @@ class Link(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
 
     def save(self, *args, **kwargs):
         self.created_date = timezone.now()
